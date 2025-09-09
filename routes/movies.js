3abb3cfe4534ec8bf606c633bcf42117490f39b8
ZacJ74 <zacharyjtx@gmail.com>
2025-09-09 13:26:44 -0500

    fixed bug, and created new movies.js file within the routes folder fixing the routing issues

---
+++ routes/movies.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/movies');
+
+// Middleware to check if the user is authenticated
+function isLoggedIn(req, res, next) {
+    if (req.session.userId) {
+        next();
+    } else {
+        res.redirect('/users/auth');
+    }
+}
+
+// RESTful Routes for Movies
+// Index Route: GET /movies
+router.get('/', isLoggedIn, moviesController.getDashboard);
+
+// New Route: GET /movies/new
+router.get('/new', isLoggedIn, moviesController.newMovieForm);
+
+// Create Route: POST /movies
+router.post('/', isLoggedIn, moviesController.createMovie);
+
+// Show Route: GET /movies/:id
+router.get('/:id', isLoggedIn, moviesController.getMovie);
+
+// Edit Route: GET /movies/:id/edit
+router.get('/:id/edit', isLoggedIn, moviesController.editMovieForm);
+
+// Update Route: PUT /movies/:id
+router.put('/:id', isLoggedIn, moviesController.updateMovie);
+
+// Delete Route: DELETE /movies/:id
+router.delete('/:id', isLoggedIn, moviesController.deleteMovie);
+
+module.exports = router;
